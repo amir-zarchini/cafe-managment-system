86489e6e7214ddb54d37d2a8e06f8d55c9907da6
Amir_zr <amzarchini@gmail.com>
2023-02-18 14:06:40 +0330

    add project in git

---
+++ src/main/java/com/example/cafemanagmentsystem/CafeManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.cafemanagmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeManagmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafeManagmentSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cafemanagmentsystem/CafeManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cafemanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeManagmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
